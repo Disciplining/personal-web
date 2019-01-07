215f55bb229b7e1f5d3c69b7a52561c49deafb5c
yanxingli <416698547@qq.com>
2019-01-07 15:14:08 +0800

    修改了配置文件

--- src/main/java/com/liyanxing/MyWebApplication.java
+++ src/main/java/com/liyanxing/MyWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.liyanxing.project.commonuser.mapper")
+@MapperScan("com.liyanxing.project.*.mapper")
 public class MyWebApplication
 {
     public static void main(String[] args)
