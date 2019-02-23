c0e3143da13c205056af6df5fc37973ad7809043
root <416698547@qq.com>
2019-02-23 21:46:47 +0800

    优化

--- src/main/java/com/liyanxing/util/SplitPage/PageSize.java
+++ src/main/java/com/liyanxing/util/SplitPage/PageSize.java
@@ -3,7 +3,7 @@ package com.liyanxing.util.SplitPage;
 //修改变量的值设置每页显示的条数
 public class PageSize
 {
-    public static final int SOFTWARE_RECOMMEND = 5; //软件推荐
-    public static final int WEBSITE_RECOMMEND = 5; //网站推荐
-    public static final int RESOURCE_DONELOAD = 5; //资源下载
+    public static final int SOFTWARE_RECOMMEND = 6; //软件推荐
+    public static final int WEBSITE_RECOMMEND = 6; //网站推荐
+    public static final int RESOURCE_DONELOAD = 6; //资源下载
 }
